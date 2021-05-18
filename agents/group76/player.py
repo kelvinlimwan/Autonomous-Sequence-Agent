f1103aae8ec74f09b841075aa0c5441ab25a46f0
Kelvin Lim <kelvinlimwan@github.com>
2021-05-18 12:00:03 +1000

    updated displayer

--- agents/group76/player.py
+++ agents/group76/player.py
@@ -6,5 +6,8 @@ class myAgent(Agent):
     def __init__(self, _id):
         super().__init__(_id)
 
+    # Given a set of available actions for the agent to execute, and
+    # a copy of the current game state (including that of the agent),
+    # select one of the actions to execute.
     def SelectAction(self, actions, game_state):
         return random.choice(actions)
