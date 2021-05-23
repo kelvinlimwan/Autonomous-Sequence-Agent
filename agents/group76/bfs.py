62f0f88922c74b52282f45a1797545ba68f4e3cc
Li He <jerelyhe@gmail.com>
2021-05-24 01:14:40 +1000

    bfs comments added

--- agents/group76/bfs.py
+++ agents/group76/bfs.py
@@ -26,6 +26,7 @@ for row in range(10):
 
 MAX = 99999
 
+# class represents a search node for BFS
 class searchNode():
     def __init__(self, state, action, depth):
         (hand, draft, chips, colour, opp_colour) = state
@@ -37,15 +38,22 @@ class searchNode():
         self.opp_colour = opp_colour
         self.depth = depth
     
+    # function returns the sucessors of the current node
     def generate_successor(self):
         successors = []
+
+        # choose a card to play
         for play_card in self.hand:
             hand_copy = copy.deepcopy(self.hand)
             hand_copy.remove(play_card)
+
+            # choose a card to draft
             for draft_card in self.draft:
                 draft_copy = copy.deepcopy(self.draft)
                 draft_copy.remove(draft_card)
                 hand_copy.append(draft_card)
+
+                # play the chosen play_card
 
                 if play_card in ['jd','jc']: #two-eyed jacks
                     # continue
@@ -81,13 +89,18 @@ class searchNode():
         return successors
  
 
+    # count the number of moves to occupy the heart of board
     def to_occupy_heart(self, chips, opp_coulor):
         heart = [chips[4][4], chips[4][5], chips[5][4], chips[5][5]]
+
+        # if there is a opp_colour in heart
+        # impossible to do so
         if opp_coulor in heart:
             return MAX
-        
         return self.num_space(heart)
 
+    # given a sequence candidate
+    # count the number of moves to complete it
     def to_complete_seq(self, seq_candidate):
         step = 5
         for i in range(len(seq_candidate) - 4):
@@ -97,6 +110,7 @@ class searchNode():
                 step = self.num_space(five_chips)
         return step
 
+    # count the number of space in the give sequence candidate
     def num_space(self, seq):
         num_space = 0
         for i in seq:
@@ -104,7 +118,7 @@ class searchNode():
                 num_space += 1
         return num_space
 
-
+    # create lines vertical, horizontal, plus 2 diagnals
     def create_lines(self, chips):
         chips = np.asarray(chips)
         lines = []
@@ -137,6 +151,8 @@ class searchNode():
                 lines.append(temp)
         return lines
     
+    # go through each line, create candidates where we can 
+    # complete a sequence
     def create_sequence_candidates(self, lines, opp_colour):
         candidates = []
 
@@ -160,28 +176,30 @@ class searchNode():
                 candidates.append(line)
         return candidates
 
+    # return reward for this state and h_value 
     def evaluate(self):
         lines = self.create_lines(self.chips)
         seq_candidates = self.create_sequence_candidates(lines, self.opp_colour)
+        
 
         steps_to_complete_sep = [self.to_complete_seq(i) for i in seq_candidates]
         steps_to_complete_sep.sort()
+        # the min number of steps to complete 2 sequences
         step_to_complete_2_seq = steps_to_complete_sep[0] + steps_to_complete_sep[1]
-
+        # mean number of steps to complete a sequence
         mean_step_to_complete = sum(steps_to_complete_sep)/len(steps_to_complete_sep)
 
         step_to_occupy_heart = self.to_occupy_heart(self.chips, self.opp_colour)
 
+        # h_value =  min step to win + mean step to complete a sequence
         h_value = min(step_to_occupy_heart, step_to_complete_2_seq) + mean_step_to_complete
-        reward = self.get_reward(step_to_occupy_heart, steps_to_complete_sep[0], steps_to_complete_sep[1])
+        # one sequence is 1 point, heart board worth 3 points by rules
+        reward = self.get_reward(step_to_occupy_heart, steps_to_complete_sep)
 
         return  reward, h_value
 
-    def get_reward(self, step_to_occupy_heart, step_to_complete_seq_1, step_to_complete_seq_2):
-        if step_to_occupy_heart == 0:
-            return 3
-
-        return int(step_to_complete_seq_1 == 0) + int(step_to_complete_seq_2 == 0)
+    def get_reward(self, step_to_occupy_heart, step_to_complete_seq):
+        return 3*int(step_to_occupy_heart == 0) + sum([int(i==0) for i in step_to_complete_seq])
 
 
 class myAgent(Agent):
@@ -189,16 +207,19 @@ class myAgent(Agent):
         super().__init__(_id)
     
     def SelectAction(self,actions,game_state):
-
+        
+        # breadth first search through the leaf nodes
+        # depth is limited to 2
         DEPTH_LIMIT = 2
 
+        # convert actions to search nodes
         temp = [(self.update_state(a, copy.deepcopy(game_state), self.id),a) for a in actions ]
         open_list = [searchNode(state, a, 1) for (state, a) in temp]
 
 
-        # breadth first search through the leaf nodes
-        # depth is limited to 2
-
+        # find the best search node in the open_list
+        # s1 is strictly better than s2 if s1 has more rewards
+        # s1 is better than s2 if they have a same reward, but s1 has a lower h_value
         best_reward, best_h_value = open_list[0].evaluate()
         best_node = open_list[0]
         visited = [best_node]
@@ -224,6 +245,7 @@ class myAgent(Agent):
 
         return best_node.action
     
+    # function execute the action and derive the resulting state
     def update_state(self, action, gs, agent_id):
         agent = gs.agents[agent_id]
         hand = agent.hand
